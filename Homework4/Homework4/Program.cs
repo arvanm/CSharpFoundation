63c0a515b077e7dcc0b6a262d17ef0eff35eb3d1
arvanm <arvanmich@gmail.com>
2016-07-19 17:59:21 -0700

    Homework4 Complete

---
+++ Homework4/Homework4/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please Enter at Number you wish to see the factorial n!");
+
+            string str = Console.ReadLine();
+
+            int number = int.Parse(str);
+            int[] arraynumber = new int[number];
+            int multiply = number;
+
+            for (int i = 1; i < arraynumber.Length; i++)
+            {
+                multiply = multiply * i;
+            }
+
+            Console.WriteLine(" The factorial number for " + number + " is: {0}", multiply);
+            Console.ReadLine();
+
+
+        }
+    }
+}
